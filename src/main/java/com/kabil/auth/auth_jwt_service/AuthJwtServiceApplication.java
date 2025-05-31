bfb5ee27f66c1e7fe2fcbb440164487f8b7cbc17
KabilBoufares <boufareskabil209@gmail.com>
2025-05-31 20:14:39 +0100

    Initial commit : Spring Boot JWT project setup

---
+++ src/main/java/com/kabil/auth/auth_jwt_service/AuthJwtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kabil.auth.auth_jwt_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthJwtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthJwtServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kabil/auth/auth_jwt_service/AuthJwtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kabil.auth.auth_jwt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthJwtServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
